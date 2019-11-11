fc8f2a91cf54697a7320f0d18f9aa395640cfd37
George Lively <george.lively@lithespeed.com>
2019-11-11 13:51:09 -0500

    removed intellij references

--- src/js/wordwrap.js
+++
@@ -1,9 +0,0 @@
-function wordWrapper(line, length) {
-    if (line.length <= length || length <= 0) return line;
-    var split = line.split('');
-    var result = [];
-    while (split.length > 0) {
-        result.push(split.splice(0, length).join(''));
-    }
-    return result.join('\n');
-}
