ac53926b7d47ff6098a98de1313c9fcc2fbba756
George Lively <george.lively@lithespeed.com>
2023-08-10 09:55:00 -0400

    Latest code for exercises (including

--- src/main/com/stringset/StringSet.java
+++ src/main/com/stringset/StringSet.java
@@ -1,27 +1,55 @@
 package main.com.stringset;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 
 public class StringSet {
 
-    HashSet<String> set = new HashSet<String>();
+    private HashSet<String> set = new HashSet<String>();
 
-    //tested
     public void add(String s) {
         set.add(s);
     }
 
-    //tested
     public int size() {
         return set.size();
     }
 
-    //tested
+    public boolean contains(String string) {
+        return set.contains(string);
+    }
+
     public void remove(String s) throws Exception {
         if (set.contains(s)) {
             set.remove(s);
         } else {
             throw new Exception();
         }
+    }
+
+    public StringSet union(StringSet otherSet) {
+        StringSet newSet = new StringSet();
+        newSet.set.addAll(this.set);
+        newSet.set.addAll(otherSet.set);
+        return newSet;
+    }
+
+    public StringSet intersection(StringSet otherSet) {
+        StringSet newSet = new StringSet();
+        for (String string : this.set) {
+            if (otherSet.contains(string)) {
+                newSet.add(string);
+            }
+        }
+        return newSet;
+    }
+
+    public void clear() {
+        set.clear();
+    }
+
+    public List<String> enumerate() {
+        return new ArrayList<>(set);
     }
 }
--- src/test/unit/com/stringset/StringSetTest.java
+++ src/test/unit/com/stringset/StringSetTest.java
@@ -1,17 +1,26 @@
 package test.unit.com.stringset;
 
 import main.com.stringset.StringSet;
-import org.junit.Before;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class StringSetTest {
     private StringSet testSubject;
 
+    @BeforeEach
+    public void setUp() {
+        testSubject = new StringSet();
+    }
+
     @Test
     public void itAddsAStringToAnEmptySet() {
-        testSubject = new StringSet();
         testSubject.add("abcd");    // act
         assertEquals(1, testSubject.size());   // assert
     }
@@ -19,7 +28,6 @@ public class StringSetTest {
     @Test
     public void itAddsMultipleStringsToANonEmptySet() {
         // arrange
-        testSubject = new StringSet();
         testSubject.add("one");
         assertEquals(1, testSubject.size());
 
@@ -33,7 +41,6 @@ public class StringSetTest {
 
     @Test
     public void itDoesNotAddTheSameStringToSet() {
-        testSubject = new StringSet();
         testSubject.add("one");
         testSubject.add("one");
         assertEquals(1, testSubject.size());
@@ -41,15 +48,74 @@ public class StringSetTest {
 
     @Test
     public void itRemovesTheOnlyElementInSet() throws Exception {
-        testSubject = new StringSet();
         testSubject.add("abcd");
         testSubject.remove("abcd");
         assertEquals(0, testSubject.size());
     }
 
-    /*@Test(expected = Exception.class)
+    @Test
+    public void itUnionsTwoSets() {
+        // arrange
+        testSubject.add("abcd");
+
+        StringSet secondSet = new StringSet();
+        secondSet.add("efgh");
+        // act
+        StringSet unionedSet = testSubject.union(secondSet);
+        // assert
+        assertEquals(2, unionedSet.size());
+        assertTrue(unionedSet.contains("efgh"));
+        assertTrue(unionedSet.contains("abcd"));
+    }
+
+    @Test
+    public void itIntersects() {
+        //arrange
+        testSubject.add("abcd");
+        testSubject.add("efgh");
+
+        StringSet secondSet = new StringSet();
+        secondSet.add("efgh");
+        secondSet.add("ijkl");
+        //act
+        StringSet intersectedSet = testSubject.intersection(secondSet);
+        //assert
+        assertEquals(1, intersectedSet.size());
+        assertTrue(intersectedSet.contains("efgh"));
+    }
+
+    @Test
+    public void itEnumeratesNonEmptySet() {
+        //arrange
+        testSubject.add("apple");
+        testSubject.add("banana");
+        testSubject.add("orange");
+
+        List<String> expected = new ArrayList<>();
+        expected.add("apple");
+        expected.add("banana");
+        expected.add("orange");
+
+        List<String> actual = testSubject.enumerate();
+        assertEquals(3, actual.size());
+        assertTrue(actual.contains("apple"));
+        assertTrue(actual.contains("banana"));
+        assertTrue(actual.contains("orange"));
+    }
+
+    @Test
     public void itAttemptsToRemoveAStringThatDoesNotExist() throws Exception {
         testSubject.add("abcd");
-        testSubject.remove("xyz");
-    }*/
+        assertThrows(Exception.class, () -> testSubject.remove("xyz"));
+    }
+
+    @Test
+    public void itClears() {
+        //arrange
+        testSubject.add("abcd");
+        //act
+        testSubject.clear();
+        //assert
+        assertEquals(0, testSubject.size());
+    }
 }
